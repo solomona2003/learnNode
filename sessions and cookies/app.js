2dea9db3dbebbad9ba89eb82f46c2e795d210187
Solomon Kahsai <abrahams17@univie.ac.at>
2019-09-17 19:19:16 +0200

    somechanges

--- sessions and cookies/app.js
+++ sessions and cookies/app.js
@@ -1,16 +1,22 @@
 const path = require('path');
-const session = require('express-session')
-const MongoDBStore = require('connect-mongodb-session')(session)
-const MONGODB_URI = 'mongodb+srv://solomon:1Godlovesme21@cluster0-kn0lh.mongodb.net/shop';
-const store = new MongoDBStore({uri: MONGODB_URI, collection: 'sessions'})
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://solomon:1Godlovesme21@cluster0-kn0lh.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,11 +27,17 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secrete', resave: false, saveUninitialized: false, store: store}));
-app.use((req, res, next)=> {
-  if(!req.session.user) {
-    console.log('------from app.js while trying to find a user that has already been placed on the req.session');
-    
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
@@ -33,10 +45,8 @@ app.use((req, res, next)=> {
       req.user = user;
       next();
     })
-    .catch(err => console.log( err));
-})
-
-
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -45,25 +55,11 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    MONGODB_URI
-  )
+  .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'sol',
-          email: 'sol@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
-    app.listen(3000);
-    console.log('connected!');
+    console.log('connected');
     
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err);
--- sessions and cookies/controllers/auth.js
+++ sessions and cookies/controllers/auth.js
@@ -1,37 +1,83 @@
-const User = require('../models/user')
-exports.getLogin = (req, res, next) => {   
-  // console.log(req.get('Cookie').split('=')[1]);
-  console.log(req.session.isLoggedIn);
-  
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.session.isLoggedIn
+    isAuthenticated: false
   });
 };
 
-
-exports.postLogin = (req, res, next) => {
-  User.findById('5d7fbc96d22a181b2363d49c')
-    .then(user => {
-      req.session.user = user;
-      req.session.isLoggedIn = true;
-      req.session.save(err=> {
-        res.redirect('/')
-
-      })
-      
-    })
-    .catch(err => console.log('from postLogin in auth.js, trying to get a user based on the user id', err));
-  // res.setHeader('Set-Cookie', 'loggedIn = true; HttpOnly')
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
 };
 
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((error)=> {
-    console.log(error);
-    console.log('loggin');
-    
-    res.redirect('/')
-  })
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
